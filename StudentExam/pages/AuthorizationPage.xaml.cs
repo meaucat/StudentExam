063f803e2a62fbd42a1944e8e9251c9fdee5a8b2
meaucat <yuuummm55@gmail.com>
2023-11-12 23:04:06 +0300

    добавил QRcode, теперь точно все

--- StudentExam/pages/AuthorizationPage.xaml.cs
+++ StudentExam/pages/AuthorizationPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using StudentExam.DB;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
 
 namespace StudentExam.pages
 {
@@ -42,9 +46,34 @@ namespace StudentExam.pages
 
             }
             else
-                LoginBtn.Background = Brushes.Red;
-            CheckLoginTextBlock.Foreground = Brushes.Red;
+                LoginBtn.Background = System.Windows.Media.Brushes.Red;
+            CheckLoginTextBlock.Foreground = System.Windows.Media.Brushes.Red;
             CheckLoginTextBlock.Text = "Что-то введено неверно, попробуйте еще раз";
+        }
+
+        private void QRcodeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Ссылка на XL таблицу
+            string soucer_xl = "https://www.youtube.com/watch?v=8Zn89VJ01jQ";
+            // Создание переменной библиотеки QRCoder
+            QRCoder.QRCodeGenerator qr = new QRCoder.QRCodeGenerator();
+            // Присваеваем значиения
+            QRCoder.QRCodeData data = qr.CreateQrCode(soucer_xl, QRCoder.QRCodeGenerator.ECCLevel.L);
+            // переводим в Qr
+            QRCoder.QRCode code = new QRCoder.QRCode(data);
+            Bitmap bitmap = code.GetGraphic(100);
+            /// Создание картинки
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                imageQr.Source = bitmapimage;
+            }
         }
     }
 }
